c5896528f9607aa604fe887b1d12809b51929272
JonasDreessen <dreessenjonas95@gmail.com>
2019-05-29 09:25:19 +0200

    get close now button

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,3 @@
-
 var d = new Date();
 
 var day = d.getDay();
@@ -7,18 +6,27 @@ var hour = d.getHours();
 
 
 
-    if (day >= 1 && day <= 5 && hour >= 8 && hour <= 19) {
-        document.getElementById("openclose").insertAdjacentHTML("beforeend", "geopend");
+if (day >= 1 && day <= 5 && hour >= 8 && hour <= 19) {
+    document.getElementById("openclose").insertAdjacentHTML("beforeend", "geopend");
 
-    } else if (day >= 6 && 7 <= day && hour >= 9 && hour <= 19) {
-        document.getElementById("openclose").insertAdjacentHTML("beforeend", "geopend");
+} else if (day >= 6 && day <= 7 && hour >= 9 && hour <= 19) {
+    document.getElementById("openclose").insertAdjacentHTML("beforeend", "geopend");
 
-    } else {
-        document.getElementById("openclose").insertAdjacentHTML("beforeend", "gesloten");
-    }
+} else {
+    document.getElementById("openclose").insertAdjacentHTML("beforeend", "gesloten");
+}
 
 
 
 var time = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
 
+var timehour = d.getHours() + ":00";
+
 document.getElementById("LiveClock").insertAdjacentHTML("beforeend", time);
+
+
+function changeClosingTime(){
+
+    document.getElementById("closingTime").innerHTML=timehour;
+}
+
